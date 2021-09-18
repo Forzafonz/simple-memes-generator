67c9228410fae0f88124dcb43f18f6baa461e26e
Anton <golubin1990@gmail.com>
2021-09-17 22:27:36 -0400

    Small fixes

--- src/Components/EditMeme.jsx
+++ src/Components/EditMeme.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useRef } from 'react'
 import { UserContext } from '../hooks/userContext'
 import Draggable from 'react-draggable';
+import useDynamicRefs from 'use-dynamic-refs';
 import './editmeme.scss';
 
 export default function(props) {
@@ -21,37 +22,55 @@ export default function(props) {
         if (input.id != event.target.id)
           return input;
       })
-    }
-    )
-  
+    })
   }
 
-  const textareas = inputList.map(input => {
-    console.log(ref)
-    console.log(ref.current.getBoundingClientRect())
-    console.log(ref.current.offsetParent.offsetLeft)
+  const handleEvent = (e, data) => {
+    console.log('Event Type', e.type);
+    console.log(e, data);
+    ref.current.click()
+  }
+
+  const handleEvent2 = (e, data) => {
+    e.target.click()
+    console.log('Event Type2', e.type);
+    console.log(e, data);
+    
+  }
+
+  const textareas = inputList.map((input, i)=> {
+
     const domRect = ref.current.getBoundingClientRect();
-    // const left = domRect.y;
     const ParentLeft = ref.current.offsetParent.offsetLeft;
     const ParentTop = ref.current.offsetParent.offsetTop;
     const left = domRect.left - ParentLeft;
     const top = domRect.top - ParentTop;
     const bottom = top + domRect.height - 32;
     const right = left + domRect.width - 180;
-    console.log({left, top, right,bottom})
     return (
       <Draggable 
       key = {input.id}
-      bounds = {{left, top, right,bottom}} >
+      bounds = {{left, top, right,bottom}}
+      // onDragEnd ={event => handleStop(event)}
+      onDrag={handleEvent}
+      onStart={handleEvent}
+      onStop={handleEvent2}
+      // onMouseDown={handleEvent}
+      // onMouseUp={handleEvent}
+      onTouchStart={handleEvent}
+      onTouchEnd={handleEvent}
+      onDrop = {handleEvent2}
+      
+      >
       <textarea
       draggable="true" 
       id = {input.id} 
       className = "input_text" 
       type = "text" 
+      defaultPosition = {{x: left, y: top}}
       value = {inputList['0']['0']}
       onChange = {(event => updateInput(event))}
-      onDoubleClick = {deleteTextArea}
-      >
+      onDoubleClick = {deleteTextArea} >
       </textarea>
       </Draggable>
     )
@@ -73,12 +92,23 @@ export default function(props) {
   const addTextArea = (event) => {
     setInputList((prev) => {
       return [...prev, {id : Date.now(), text: ""}]
-    }
-    )
+    })
   }
 
- 
-  console.log(textareas)
+  const saveText = (event) => {
+    console.log(inputList)
+    console.log(textareas[0])
+    const data = textareas.map(area => {
+      const text = inputList.map(item => {
+        if (item.id == area.key){
+          return item.text;
+        }
+      })
+      return {bounds: area.props.bounds, text}
+    })
+    console.log("DATA:", data)
+    // dispatch({type: "updatetext", values: { text: inputList}})
+  }
 
   const [{id, name, url}] = element;
   return (
@@ -92,8 +122,7 @@ export default function(props) {
         </div>
         <br/>
         <button onClick ={addTextArea}>Add Text</button>
-        <button onClick ={deleteTextArea}>Delete Text</button>
-        <button>Save</button>
+        <button onClick = {saveText}>Save</button>
         <button>Reset</button>
       </div>
     </div>
